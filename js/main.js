8417a360e0fecc8ef09e8809dab442bd358f7ad9
yukimemi <yukimemi@gmail.com>
2015-02-15 08:26:56 +0900

    publish: Change roots to Hugo.
    generated from commit 83380e500a064f385f317290693b6a0e5cfa5b13

--- js/main.js
+++ js/main.js
@@ -1,39 +1,10 @@
-(function() {
-  console.log("hello from a require'd coffee file <-- via assets/js/_helper.coffee");
+$(function() {
+  console.log("begin");
+  $('pre').addClass('prettyprint linenums');
+  prettyPrint();
 
-}).call(this);
+  $('#post h3').addClass('amber-text text-darken-4');
+  $('#post h4').addClass('teal-text text-accent-3');
+  $('#post h5').addClass('light-blue-text text-accent-3');
+});
 
-(function() {
-  var jq, js;
-
-  jq = ["http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"];
-
-  js = ["/bower_components/google-code-prettify/src/prettify.js", "/bower_components/jquery.simplePagination/jquery.simplePagination.js"];
-
-  require(jq, function() {
-    return require(js, function() {
-      return $(function() {
-        var items, numItems, perPage;
-        console.log('jquery loaded, dom ready <-- via assets/js/main.coffee');
-        items = $('#posts li');
-        numItems = items.length;
-        perPage = 21;
-        items.slice(0, perPage).show();
-        $('.pagination').pagination({
-          items: numItems,
-          itemsOnPage: perPage,
-          cssStyle: "light-theme",
-          onPageClick: function(pageNumber) {
-            var showFrom, showTo;
-            showFrom = perPage * (pageNumber - 1);
-            showTo = showFrom + perPage;
-            return items.hide().slice(showFrom, showTo).show();
-          }
-        });
-        $('pre').addClass('prettyprint linenums');
-        return prettyPrint();
-      });
-    });
-  });
-
-}).call(this);
