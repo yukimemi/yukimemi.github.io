a29bf7436c74a555c12a936dc11bd3fe127f67cc
yukimemi <yukimemi@gmail.com>
2015-05-04 06:21:28 +0900

    publish: Mayu for IME off in vim
    generated from commit de77b524a4cc33a2a214d2dfbbddbcb6a4eff3ac

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@ $(function() {
   $('#post h5').addClass('light-blue-text text-accent-3');
 });
 
+// add line number to gist
+$('.gist').each(function() {
+  $('.line',this).each(function(i, e) {
+    $(this).prepend( $('<div/>').attr('unselectable','on').css({
+      'float' : 'left',
+      'width': '30px',
+      'font-weight' : 'bold',
+      'color': 'grey',
+      '-moz-user-select': 'none',
+      '-webkit-user-select': 'none'
+    }).text(++i) );
+  });
+});
+
