773314e85b4ea77412a7b1c024469d3e63c3116a
yukimemi <yukimemi@gmail.com>
2016-12-10 00:27:18 +0900

    publish: :+1: Post dein and vim-singleton.
    generated from commit 449d0ca9c104354dbfc7a6cfbfccb72d9286a7a9

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 $(function() {
   console.log("begin");
-  $('pre').addClass('prettyprint linenums');
-  prettyPrint();
 
   $('#post h3').addClass('amber-text text-darken-4');
   $('#post h4').addClass('teal-text text-accent-3');
@@ -10,15 +8,14 @@ $(function() {
 
 // add line number to gist
 $('.gist').each(function() {
-  $('.line',this).each(function(i, e) {
-    $(this).prepend( $('<div/>').attr('unselectable','on').css({
-      'float' : 'left',
+  $('.line', this).each(function(i, e) {
+    $(this).prepend($('<div/>').attr('unselectable', 'on').css({
+      'float': 'left',
       'width': '30px',
-      'font-weight' : 'bold',
+      'font-weight': 'bold',
       'color': 'grey',
       '-moz-user-select': 'none',
       '-webkit-user-select': 'none'
-    }).text(++i) );
+    }).text(++i));
   });
 });
-
